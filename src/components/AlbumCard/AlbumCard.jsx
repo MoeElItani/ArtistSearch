81c92e8c8fa5d980c1c7114dea954353667ba99c
MoeElItani <mhmd98itani@gmail.com>
2023-03-27 17:14:16 +0300

    update album cards

--- src/components/AlbumCard/AlbumCard.jsx
+++ src/components/AlbumCard/AlbumCard.jsx
@@ -1,18 +1,35 @@
 const AlbumCard = ({ album }) => {
+   console.log(album)
    return (
       //  Card Component of an album
       <div
          key={album.artistId}
-         className='w-1/2 lg:w-1/4 px-4'
+         className='w-1/2 lg:w-1/4 px-4 mb-8'
       >
          <img
             src={album.images[0].url}
             alt={album.name}
-            className='mb-3 w-[60%] rounded shadow-md'
+            className='mb-3 w-[80%] rounded shadow-md'
          />
          <h2 className='font-bold text-xl mb-2'>
             {album.name}
          </h2>
+            <p>{album.artists[0].name}</p>
+            <p>{album.release_date}</p>
+            <p>
+               {album.total_tracks}{' '}
+               {album.total_tracks < 2 ? 'track' : 'tracks'}
+            </p>
+         <button className='w-[80%] h-14 bg-[#1DB954]'>
+            <a
+               href={album.external_urls.spotify}
+               target='_blank'
+               rel='noreferrer'
+               className='text-white font-semibold'
+            >
+               Preview on Spotify
+            </a>
+         </button>
       </div>
    )
 }
